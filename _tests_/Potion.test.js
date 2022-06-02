be6b91b43f040c576e8706627082cd7fe9eb5214
Tom hoffman <tomhoffmanco@gmail.com>
2022-06-02 17:50:02 -0400

    refractor of handler for potion

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -6,3 +6,15 @@ test("creates a health potion object", () => {
   expect(potion.name).toBe("health");
   expect(potion.value).toEqual(expect.any(Number));
 });
+
+// Note that this time, we won't check to see if the potion has the name value of health. If we wanted to, we could have written a test that checks to see if name is health, strength, or agility.
+
+test("creates a random potion object", () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+module.exports = Potion;
